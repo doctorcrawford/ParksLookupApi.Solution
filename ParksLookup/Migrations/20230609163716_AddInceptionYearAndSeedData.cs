c28d6709cf62c8b535bf5d01a988fbec539f40e2
Kyle Crawford <kcraw217@gmail.com>
2023-06-09 09:38:12 -0700

    add seed data

---
+++ ParksLookup/Migrations/20230609163716_AddInceptionYearAndSeedData.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ParksLookup.Migrations
+{
+    public partial class AddInceptionYearAndSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Type",
+                table: "Parks",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.InsertData(
+                table: "Parks",
+                columns: new[] { "ParkId", "FeaturedAnimal", "InceptionYear", "Name", "State", "Type" },
+                values: new object[,]
+                {
+                    { 1, "Elk", 1872, "Yellowstone", "Wyoming", "National" },
+                    { 2, "Golden-Mantled Ground Squirrel", 1902, "Crater Lake", "Oregon", "National" },
+                    { 3, "Desert Bighorn Sheep", 1964, "Canyonlands", "Utah", "National" },
+                    { 4, "Great Horned Owl", 1958, "Oswald West State Park", "Oregon", "State" },
+                    { 5, "Red-Shouldered Hawks", 1929, "Fahnestock", "New York", "State" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 5);
+
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Parks");
+        }
+    }
+}
--- ParksLookup/Models/ParksLookupApiContext.cs
+++ ParksLookup/Models/ParksLookupApiContext.cs
@@ -8,6 +8,17 @@ public class ParksLookupApiContext : DbContext
 
   public ParksLookupApiContext(DbContextOptions<ParksLookupApiContext> options) : base(options)
   {
+  }
 
+  protected override void OnModelCreating(ModelBuilder Builder)
+  {
+    Builder.Entity<Park>()
+      .HasData(
+        new Park { ParkId = 1, Name = "Yellowstone", Type = ParkType.National.ToString(), State = "Wyoming", InceptionYear = 1872, FeaturedAnimal = "Elk"},
+        new Park { ParkId = 2, Name = "Crater Lake", Type = ParkType.National.ToString(), State = "Oregon", InceptionYear = 1902, FeaturedAnimal = "Golden-Mantled Ground Squirrel"},
+        new Park { ParkId = 3, Name = "Canyonlands", Type = ParkType.National.ToString(), State = "Utah", InceptionYear = 1964, FeaturedAnimal = "Desert Bighorn Sheep"},
+        new Park { ParkId = 4, Name = "Oswald West State Park", Type = ParkType.State.ToString(), State = "Oregon", InceptionYear = 1958, FeaturedAnimal = "Great Horned Owl"},
+        new Park { ParkId = 5, Name = "Fahnestock", Type = ParkType.State.ToString(), State = "New York", InceptionYear = 1929, FeaturedAnimal = "Red-Shouldered Hawks"}
+      );
   }
 }
