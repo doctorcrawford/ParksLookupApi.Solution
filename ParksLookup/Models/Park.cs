ae9ce35d2fa0b5140cfa39b248ae16c99fab75cf
Kyle Crawford <kcraw217@gmail.com>
2023-06-09 09:38:03 -0700

    add Inception Year

--- ParksLookup/Models/Park.cs
+++ ParksLookup/Models/Park.cs
@@ -4,7 +4,15 @@ public class Park
 {
   public int ParkId { get; set; }
   public string Name { get; set; }
+  public string Type { get; set; }
   public string State { get; set; }
-  public string FeaturedAnimal { get; set; }
   public int InceptionYear { get; set; }
+  public string FeaturedAnimal { get; set; }
+}
+
+public enum ParkType {
+  Local,
+  County,
+  State,
+  National
 }
