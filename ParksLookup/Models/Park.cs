f0b195ecc7e5cbc781dc05f5b07801c348d14707
Kyle Crawford <kcraw217@gmail.com>
2023-06-09 11:43:09 -0700

    add input validation

--- ParksLookup/Models/Park.cs
+++ ParksLookup/Models/Park.cs
@@ -1,16 +1,32 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ParksLookupApi.Models;
 
 public class Park
 {
   public int ParkId { get; set; }
+  [Required]
+  [StringLength(1, ErrorMessage = "Name must be at least 1 character.")]
   public string Name { get; set; }
+  [Required]
+  [StringLength(2, ErrorMessage = "Type must be at least 2 characters.")]
   public string Type { get; set; }
+  [Required]
+  [StringLength(2, ErrorMessage = "State must be at least 2 characters.")]
   public string State { get; set; }
+  [Required]
   public int InceptionYear { get; set; }
   public string FeaturedAnimal { get; set; }
+
+  public static int CurrentYear()
+  {
+    return DateTime.Now.Year;
+  }
 }
 
-public enum ParkType {
+
+public enum ParkType
+{
   Local,
   County,
   State,
