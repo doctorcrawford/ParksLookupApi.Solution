bdb3c6a2f8a762a50e1adda21ddf151858c0bb56
Kyle Crawford <kcraw217@gmail.com>
2023-06-09 13:50:48 -0700

    WIP keep getting 'data too long' error for 'type'

--- ParksLookup/Models/ParksLookupApiContext.cs
+++ ParksLookup/Models/ParksLookupApiContext.cs
@@ -1,24 +1,26 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace ParksLookupApi.Models;
 
-public class ParksLookupApiContext : DbContext
+public class ParksLookupApiContext : IdentityDbContext<ApplicationUser>
 {
   public DbSet<Park> Parks { get; set; }
-
-  public ParksLookupApiContext(DbContextOptions<ParksLookupApiContext> options) : base(options)
+  public ParksLookupApiContext(DbContextOptions options) : base(options)
   {
   }
 
-  protected override void OnModelCreating(ModelBuilder Builder)
+  protected override void OnModelCreating(ModelBuilder builder)
   {
-    Builder.Entity<Park>()
+    builder.Entity<Park>()
       .HasData(
-        new Park { ParkId = 1, Name = "Yellowstone", Type = ParkType.National.ToString(), State = "Wyoming", InceptionYear = 1872, FeaturedAnimal = "Elk"},
-        new Park { ParkId = 2, Name = "Crater Lake", Type = ParkType.National.ToString(), State = "Oregon", InceptionYear = 1902, FeaturedAnimal = "Golden-Mantled Ground Squirrel"},
-        new Park { ParkId = 3, Name = "Canyonlands", Type = ParkType.National.ToString(), State = "Utah", InceptionYear = 1964, FeaturedAnimal = "Desert Bighorn Sheep"},
-        new Park { ParkId = 4, Name = "Oswald West State Park", Type = ParkType.State.ToString(), State = "Oregon", InceptionYear = 1958, FeaturedAnimal = "Great Horned Owl"},
-        new Park { ParkId = 5, Name = "Fahnestock", Type = ParkType.State.ToString(), State = "New York", InceptionYear = 1929, FeaturedAnimal = "Red-Shouldered Hawks"}
+        new Park { ParkId = 1, Name = "Yellowstone", Type = ParkType.National.ToString(), State = "Wyoming", InceptionYear = 1872, FeaturedAnimal = "Elk" },
+        new Park { ParkId = 2, Name = "Crater Lake", Type = ParkType.National.ToString(), State = "Oregon", InceptionYear = 1902, FeaturedAnimal = "Golden-Mantled Ground Squirrel" },
+        new Park { ParkId = 3, Name = "Canyonlands", Type = ParkType.National.ToString(), State = "Utah", InceptionYear = 1964, FeaturedAnimal = "Desert Bighorn Sheep" },
+        new Park { ParkId = 4, Name = "Oswald West State Park", Type = ParkType.State.ToString(), State = "Oregon", InceptionYear = 1958, FeaturedAnimal = "Great Horned Owl" },
+        new Park { ParkId = 5, Name = "Fahnestock", Type = ParkType.State.ToString(), State = "New York", InceptionYear = 1929, FeaturedAnimal = "Red-Shouldered Hawks" }
       );
+
+    base.OnModelCreating(builder);
   }
 }
