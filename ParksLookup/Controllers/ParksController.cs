b0401869119bb46461ecea47e23ff1d9564ed66b
Kyle Crawford <kcraw217@gmail.com>
2023-06-09 10:12:47 -0700

    add GetAllParksAsync and GetParkAsync

--- ParksLookup/Controllers/ParksController.cs
+++ ParksLookup/Controllers/ParksController.cs
@@ -15,5 +15,23 @@ public class ParksController : ControllerBase
     _db = db;
   }
 
-  
+  // GET api/parks
+  [HttpGet]
+  public async Task<ActionResult<IEnumerable<Park>>> GetAllParksAsync()
+  {
+    return await _db.Parks.ToListAsync();
+  }
+
+  [HttpGet("{id}")]
+  public async Task<ActionResult<Park>> GetParkAsync(int id)
+  {
+    var park = await _db.Parks.FindAsync(id);
+
+    if (park == null)
+    {
+      return NotFound();
+    }
+
+    return park;
+  }
 }
